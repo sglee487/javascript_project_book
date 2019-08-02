30649ade86d252fc63302e4a4514a664f9132a39
sglee487 <sglee487@gmail.com>
2019-08-02 13:04:23 +0900

    7장 라이트 박스 프로젝트. jQuery 코드로 변환 후.

--- 7.light_box/main.js
+++ 7.light_box/main.js
@@ -1,28 +1,49 @@
-// 인디케이터 초기화
-var indicator = document.querySelectorAll('.indicator button');
-var lightbox = document.querySelector('#lightbox');
-var block = document.querySelector('#block');
+$(function () {
 
-// 라이트 박스 표시
-function lightbox_open(num) {
-    lightbox.setAttribute('class','active');
-    block.setAttribute('class','active');
-    change_img(num);
-    indicator[num-1].focus();
-}
+    // 변수 초기화
+    var $indicator = $('.indicator button');
+    var $lightbox = $('#lightbox');
+    var $block = $('#block');
+
+    // 라이트 박스 표시
+    function lightbox_open(num) {
+        $lightbox.addClass('active');
+        $block.addClass('active')
+        change_img(num);
+        $indicator.eq(num).focus();
+    }
 
 // 라이트 박스 닫기
-function lightbox_close() {
-    lightbox.removeAttribute('class');
-    block.removeAttribute('class');
-}
+    function lightbox_close() {
+        $lightbox.removeAttr('class');
+        $block.removeAttr('class');
+    }
 
 // 이미지 전환 표시 함수
-function change_img(val) {
-    var imgs = document.querySelectorAll('figure > img');
+    function change_img(val) {
+        var $imgs = $('figure > img');
 
-    for (var i=0; i<imgs.length;i++){
-        imgs[i].removeAttribute('class');
+        for (var i=0; i<$imgs.length;i++){
+            $imgs.eq(i).removeAttr('class');
+        }
+        $imgs.eq(val).attr('class','active');
     }
-    imgs[val-1].setAttribute('class','active');
-}
+
+    // 이미지 목록 click 이벤트 | 라이트 박스 열기
+    $('img.thumb').click(function () {
+        var img_num = $(this).index() - 1; // index() 메서드는 현재 이미지 객체의 배열 번호를 반환합니다.
+        lightbox_open(img_num);
+    });
+
+    // 라이트 박스 닫기 버튼 click 이벤트
+    $('.btn-close').click(function () {
+        lightbox_close();
+    });
+
+    // 인디케이터 클릭 시 click 이벤트 | 라이트 박스 이미지 변경
+    $indicator.click(function () {
+        var img_num = $(this).index();
+        change_img(img_num);
+    });
+
+});
